fcb189f4e96c4d95c591d8c95fdaa4120b49a5be
bpoc-mark <markariel.maata@gmail.com>
2021-10-20 09:59:35 +0800

    master template

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php get_header();?>
+<div class="page__content top">
+    <section class="sect_1">
+        <div class="l-wrap">
+            1
+        </div>    
+    </section>
+    <section>2</section>
+    <section>3</section>
+    <section>4</section>
+    <section>5</section>
+    <section>6</section>
+    <section>7</section>
+    <section>8</section>
+    <section>9</section>
+    <section>10</section>
+    <section>11</section>
+    <section>12</section>
+</div>
+<?php get_footer();?>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('title');?></title>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/cross-platform-yu-gothic@0.1.1/cross-platform-yu-gothic.min.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/release/css/style.css">
+    
+    <?php wp_head();?>	
+</head>
+<body <?php body_class(); ?>>
+    
+<main>
+    <div class="page__header">
+        <p class="yugo_b" style="font-size:11px;font-family:cross-platform-yu-gothic; font-weight: 700; ">事業内容</p>
+        <p class="yugo_b" style="font-size:14px;font-family: century-gothic, sans-serif; font-weight: 400;letter-spacing: .32em;">OUR ENTERPRISES</p>
+        <p class="yugo_b" style="font-size:30px;font-family: yu-mincho-pr6n, sans-serif;; font-weight: 500;">丸の内ルーフの <br> 4つの事業をご紹介いたします。</p>
+    </div>
+    
