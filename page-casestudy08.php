a975a62d002ab18a73e00e1bd3b2bfc5786ffa12
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-10-21 18:00:47 +0800

    setup casestudy08 page

---
+++ page-casestudy08.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: casestudy08 */  ?>
+<?php get_header();?>
+
+<div class="page__content case_study08">
+    <section class="sect_1">
+        <div class="l-wrap">
+            <h1>All HAIL THE KING</h1>
+        </div>    
+    </section>
+    <section>2</section>
+    <section>3</section>
+    <section>4</section>
+    <section>5</section>
+    <section>6</section>
+    <section>7</section>
+    <section>8</section>
+    <section>9</section>
+    <section>10</section>
+    <section>11</section>
+    <section>12</section>
+</div>
+<?php get_footer();?>
