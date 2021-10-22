963d2df6c907ac88971aff3a6616ae63f35d787d
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-10-22 09:45:28 +0800

    modify changes on sect1 casestudy08

--- page-casestudy08.php
+++ page-casestudy08.php
@@ -1,13 +1,21 @@
 <?php /* Template Name: casestudy08 */  ?>
 <?php get_header();?>
 
-<div class="page__content case_study08">
+<div class="page__content ct08">
     <section class="sect_1">
         <div class="l-wrap">
-            <h1>All HAIL THE KING</h1>
+           <div class="sect_1--content">
+                <p class="sect_1--ttl">Case Studies</p>
+                <p class="sect_1--ttl-body">
+                コンクリ打ちっ放しのデザインを活かした外観リフォームを <br>
+                実施し賃料アップと満室稼働に成功した事例。
+                </p>
+                <p class="sect_1--ttl-sub">2000年竣工、5階建、鉄筋コンクリート</p>
+           </div>
         </div>    
     </section>
-    <section>2</section>
+
+    <!-- <section>2</section>
     <section>3</section>
     <section>4</section>
     <section>5</section>
@@ -17,6 +25,6 @@
     <section>9</section>
     <section>10</section>
     <section>11</section>
-    <section>12</section>
+    <section>12</section> -->
 </div>
 <?php get_footer();?>
