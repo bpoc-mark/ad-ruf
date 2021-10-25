eeed0f030a525a175d3ff26e53c244305c0fc135
roi-louise <roi@bpoc.co.jp>
2021-10-25 18:00:58 +0800

    change:update interview page

---
+++ page-interview.php
@@ -0,0 +1,86 @@
+<?php
+
+/* Template Name: interview */
+
+get_header();?>
+
+<div class="interview-sec1">
+    <div class="interview-sec1-cols">
+        <div class="interview-sec1-cols-1">
+            <div class="l-wrap">
+                <div class="interview-sec1-cols-1-hdr">
+                    <p class="interview-sec1-cols-1-hdr-1">Interview</p>
+                    <div class="interview-sec1-cols-1-hdr-sub1">
+                        <p class="interview-sec1-cols-1-hdr-sub">この文章はダミーです</p>
+                        <p class="interview-sec1-cols-1-hdr-sub">この文章はダミーです</p>
+                    </div> 
+                    <div class="interview-sec1-cols-1-hdr-sub2">
+                        <p class="interview-sec1-cols-1-hdr-sub">この文章はダミーですこの文章はダミーですこの文章はダミーです</p>
+                        <p class="interview-sec1-cols-1-hdr-sub">この文章はダミーですこの文章はダミーですこの文章はダミーです</p>
+                    </div>             
+                </div>
+            </div>
+        </div>
+        <div class="interview-sec1-cols-2">
+            <div class="l-wrap">
+                <div class="interview-sec1-cols-2-cntnt">
+                <div class="interview-sec1-cols-2-cntnt-1">
+                    
+                            <p class="interview-sec1-cols-2-cntnt-1-text1">
+                                この文章はダミーです
+                                <br>
+                                この文章はダミーです
+                            </p>
+                            <p class="interview-sec1-cols-2-cntnt-1-text2">
+                                Dummy textDummy textDummy textDummy text
+                                <br>
+                                Dummy textDummy text
+                                <br>
+                                Dummy textDummy textDummy text
+                            </p>
+                            <p class="interview-sec1-cols-2-cntnt-1-text3">
+                            この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーです
+                            </p>
+
+                            <div class="interview-sec1-cols-2-cntnt-1-biography">
+                                <div class="interview-sec1-cols-2-cntnt-1-biography-hdr">
+                                    <p>代表取締役 木村武史の略歴</p>
+                                </div>
+                                <div class="interview-sec1-cols-2-cntnt-1-biography-yrs">
+                                    <div class="biography-yrs-cntnt">
+                                        <p>0000年</p>
+                                        <p>この文章はダミーです</p>
+                                    </div>
+                                    <div class="biography-yrs-cntnt">
+                                        <p>0000年</p>
+                                        <p>この文章はダミーです</p>
+                                    </div>
+                                    <div class="biography-yrs-cntnt">
+                                        <p>0000年</p>
+                                        <p>この文章はダミーです</p>
+                                    </div>
+                                    <div class="biography-yrs-cntnt">
+                                        <p>0000年</p>
+                                        <p>この文章はダミーです</p>
+                                    </div>
+                                    <div class="biography-yrs-cntnt">
+                                        <p>0000年</p>
+                                        <p>この文章はダミーです</p>
+                                    </div>
+                                    <div class="biography-yrs-cntnt">
+                                        <p>0000年</p>
+                                        <p>この文章はダミーです</p>
+                                    </div> 
+                                </div>
+                            </div>
+                    
+                </div>
+
+                <div class="interview-sec1-cols-2-cntnt-2">
+                        <img src="<?php echo get_template_directory_uri();?>/release/image/interview/sec1_img.png" alt="">
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
