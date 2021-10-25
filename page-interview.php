2eeda200821ff0c5565a41ee19a864b007b1335f
roi-louise <roi@bpoc.co.jp>
2021-10-26 00:33:32 +0800

    change:update section 1 interview page

--- page-interview.php
+++ page-interview.php
@@ -4,83 +4,71 @@
 
 get_header();?>
 
-<div class="interview-sec1">
-    <div class="interview-sec1-cols">
-        <div class="interview-sec1-cols-1">
-            <div class="l-wrap">
-                <div class="interview-sec1-cols-1-hdr">
-                    <p class="interview-sec1-cols-1-hdr-1">Interview</p>
-                    <div class="interview-sec1-cols-1-hdr-sub1">
-                        <p class="interview-sec1-cols-1-hdr-sub">この文章はダミーです</p>
-                        <p class="interview-sec1-cols-1-hdr-sub">この文章はダミーです</p>
-                    </div> 
-                    <div class="interview-sec1-cols-1-hdr-sub2">
-                        <p class="interview-sec1-cols-1-hdr-sub">この文章はダミーですこの文章はダミーですこの文章はダミーです</p>
-                        <p class="interview-sec1-cols-1-hdr-sub">この文章はダミーですこの文章はダミーですこの文章はダミーです</p>
-                    </div>             
-                </div>
-            </div>
-        </div>
-        <div class="interview-sec1-cols-2">
-            <div class="l-wrap">
-                <div class="interview-sec1-cols-2-cntnt">
-                <div class="interview-sec1-cols-2-cntnt-1">
-                    
-                            <p class="interview-sec1-cols-2-cntnt-1-text1">
-                                この文章はダミーです
-                                <br>
-                                この文章はダミーです
-                            </p>
-                            <p class="interview-sec1-cols-2-cntnt-1-text2">
-                                Dummy textDummy textDummy textDummy text
-                                <br>
-                                Dummy textDummy text
-                                <br>
-                                Dummy textDummy textDummy text
-                            </p>
-                            <p class="interview-sec1-cols-2-cntnt-1-text3">
-                            この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーです
-                            </p>
+<div class="page__content interview">
+   <div class="section_1">
+       <div class="l-wrap">
+            <img class="bg-1" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/bg-1.png" alt="">
+            <p class="section_1-ttl">Interview</p>
+            <p class="section_1-sub1">この文章はダミーです<br>この文章はダミーです</p>
+            <p class="section_1-sub2">この文章はダミーですこの文章はダミーですこの文章はダミーです<br>この文章はダミーですこの文章はダミーですこの文章はダミーです</p>
+       </div>
+       <img class="bg-2" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/bg-2.png" alt="">
 
-                            <div class="interview-sec1-cols-2-cntnt-1-biography">
-                                <div class="interview-sec1-cols-2-cntnt-1-biography-hdr">
-                                    <p>代表取締役 木村武史の略歴</p>
-                                </div>
-                                <div class="interview-sec1-cols-2-cntnt-1-biography-yrs">
-                                    <div class="biography-yrs-cntnt">
-                                        <p>0000年</p>
-                                        <p>この文章はダミーです</p>
-                                    </div>
-                                    <div class="biography-yrs-cntnt">
-                                        <p>0000年</p>
-                                        <p>この文章はダミーです</p>
-                                    </div>
-                                    <div class="biography-yrs-cntnt">
-                                        <p>0000年</p>
-                                        <p>この文章はダミーです</p>
-                                    </div>
-                                    <div class="biography-yrs-cntnt">
-                                        <p>0000年</p>
-                                        <p>この文章はダミーです</p>
-                                    </div>
-                                    <div class="biography-yrs-cntnt">
-                                        <p>0000年</p>
-                                        <p>この文章はダミーです</p>
-                                    </div>
-                                    <div class="biography-yrs-cntnt">
-                                        <p>0000年</p>
-                                        <p>この文章はダミーです</p>
-                                    </div> 
-                                </div>
+       <!-- content-1 -->
+        <div class="section_1-cntnt">
+            <div class="l-wrap">
+                <div class="u-d-f u-jc-sb">
+                    <div class="section_1-cntnt-1">
+                        <p class="section_1-cntnt-1-hdr">この文章はダミーです<br>この文章はダミーです</p>
+                        <p class="section_1-cntnt-1-txt1">Dummy textDummy textDummy textDummy text<br> Dummy textDummy text <br>Dummy textDummy textDummytext</p>
+                        <p class="section_1-cntnt-1-txt2">
+                            この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーです</p>
+                        <div class="section_1-cntnt-1-biography">
+                            <p class="section_1-cntnt-1-biography-hdr">代表取締役 木村武史の略歴</p>
+                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
                             </div>
-                    
-                </div>
+                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                            </div>
+                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                            </div>
+                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                            </div>
+                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                            </div>
+                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                            </div>
+                        </div>
+                    </div>
 
-                <div class="interview-sec1-cols-2-cntnt-2">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/interview/sec1_img.png" alt="">
-                </div>
+                    <div class="section_1-cntnt-2">
+                        <p class="section_1-cntnt-2-txt1">代表取締役</p>
+                        <p class="section_1-cntnt-2-txt2">木村 武史</p>
+                    </div>
+                </div> 
             </div>
+            <img class="section_1-img" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/sec1_img.png" alt="">
+        </div>
+   </div>
+
+   <div class="section_2">
+        <div class="l-wrap">
+            <div class="section_2-cntnt">
+                <div class="section_2-cntnt-row1">
+                    <img src="<?php echo get_template_directory_uri();?>/release/image/page/interview/s2_img.png" alt="">
+                </div>
             </div>
         </div>
-    </div>
+   </div>
 </div>
