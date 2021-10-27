0d97d4e29fd5a3abb9b9c34a2c822d7106996a2f
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-10-27 18:02:40 +0800

    changed: work on our-enterprises page (on progress)

---
+++ page-our-enterprises.php
@@ -0,0 +1,53 @@
+<?php get_header();
+
+$imagedir = get_template_directory_uri();
+
+?>
+<div class="page__content oe">
+    <div class="oe__section_1">
+        <!-- //CLIP -->
+        <div class="clip-top"></div>
+
+        <div class="l-wrap">
+            <img class="oe__bg_1" src="<?php echo $imagedir ?>/release/image/page/privacy-policy/pp_bg_1.png" alt="">
+            <p class="oe__ttl">OUR ENTERPRISES</p>
+            <h1 class="oe__title">丸の内ルーフの4つの 事業紹介</h1>
+            <p class="oe__sub">私たち丸の内ルーフはコンサルティング会社ではありません。<br>
+                自社で収益不動産を所有する「プロのオーナー」として、オーナー様と同じ目線でサービスを提供します。</p>
+        </div>
+        <img class="oe__bg_2" src="<?php echo $imagedir ?>/release/image/background/rect_big.png" alt="">
+
+        <div class="oe-main-content">
+            <div class="l-wrap">
+                <div class="row">
+                    <div class="col heading">
+                        <p class="heading01">収益不動産の売買</p>
+                        <p class="heading02">Real Estate Purchase</p>
+                        <p class="heading03">丸の内ルーフは、あなたの大切な一棟マンションやアパートな<br>どの収益不動産を最大限に評価して買取いたします。</p>
+
+                        <div class="col--box">
+                            <img src="<?php echo $imagedir ?>/release/image/page/ourenterprises/oe-img-1.png" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col heading">
+                        <p class="heading01">収益不動産の管理</p>
+                        <p class="heading02">Real Estate Management</p>
+                        <p class="heading03 ls007">丸の内ルーフは、豊富なバリューアップの経験を武器に最適な不動<br>産管理を行い、お預かりした大切な収益不動産の価値を高めます。</p>
+
+                        <div class="col--box">
+                            <img src="<?php echo $imagedir ?>/release/image/page/ourenterprises/oe-img-2.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+    </div>
+</div>
+<?php get_footer(); ?>
