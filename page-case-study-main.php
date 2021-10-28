9324797405c7a7a912b8bd46597a2e54d94a902e
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-10-28 15:44:25 +0800

    set up case-study main page

---
+++ page-case-study-main.php
@@ -0,0 +1,71 @@
+<?php get_header();
+
+$imagedir = get_template_directory_uri();
+
+?>
+<div class="page__content case csm">
+    <div class="section_1">
+        <!-- //CLIP -->
+        <div class="clip-top"></div>
+        <div class="c-clipbg__CS"></div>
+        <div class="l-wrap">
+            <img class="bg_1" src="<?php echo $imagedir ?>/release/image/page/case/bg_1.png" alt="">
+            <p class="section_1__ttl">Case Studies</p>
+            <h1 class="c-case01__title">丸の内ルーフのバリューアップは○○○○○</h1>
+            <p class="c-case01__sub">私たち丸の内ルーフは○○○○○○○○○○○○○○○○○。<br>
+                ○○○○○○○○○○○○○(特徴が入ります)。</p>
+        </div>
+        <!-- //CONTENT 1 -->
+        <div class="c-case--items">
+            <div class="l-wrap">
+                <div class="c-case--items__contents">
+                    <div class="csm--contents">
+                        
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="c-clipbg__CS white_bg"></div>
+        </div>
+
+        <!-- //CONTENT 2 -->
+        <div class="c-case--items">
+            <div class="l-wrap">
+                <img class="bg_2" src="<?php echo $imagedir ?>/release/image/page/case/bg_2.png" alt="">
+                <div class="c-case--items__contents">
+                    <img class="bg_3" src="<?php echo $imagedir ?>/release/image/page/case/bg_3.png" alt="">
+                    <div class="c-case--items__contents--row_1">
+                        <div class="img-1">
+                            <img src="<?php echo $imagedir ?>/release/image/page/case/case_2-img1.png" alt="">
+                            <div class="img_box">
+                                <div class="img_box__txt">
+                                    <p class="img_box__ttl">Before</p>
+                                    <p class="img_box__sub">外観</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="img-2">
+                            <img src="<?php echo $imagedir ?>/release/image/page/case/case_2-img2.png" alt="">
+                            <div class="img_box">
+                                <div class="img_box__txt">
+                                    <p class="img_box__ttl">After</p>
+                                    <p class="img_box__sub">外観</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="c-case--items__contents--row_2">
+                        <div class="c-case--items__contents--row_2__txt">
+                            <p class="c-case--items__contents--row_2__txt ttl">1995年竣工、4階建、鉄筋コンクリート</p>
+                            <p class="c-case--items__contents--row_2__txt sub">エントランスの<br>ビフォーアフター</p>
+                            <p class="c-case--items__contents--row_2__txt desc">エントランス周りはタイルやドアを全て交換いたしました。自転車置き場をアップグレードした上で、花壇も整えました。</p>
+                        </div>
+                    </div>
+                </div>
+                <img class="bg_7" src="<?php echo $imagedir ?>/release/image/page/case/bg_7.png" alt="">
+            </div>
+        </div>
+
+        <!-- //CONTENT 6 -->
+    </div>
+    <?php get_footer(); ?>
