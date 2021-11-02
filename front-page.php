f47b924cd317fab4bfc265b2a32bc9eecc1e86be
arsua-greg <gregarsua@bpoc.co.jp>
2021-11-02 20:14:45 +0800

    Add: contact form

--- front-page.php
+++ front-page.php
@@ -1,12 +1,6 @@
 <?php get_header(); ?>
 <div class="page__content top">
     <section class="sect_1">
-<<<<<<< HEAD
-        <div class="l-wrap">
-            1
-        </div>
-=======
->>>>>>> f9672cf0fcac06317a2c6bf49b4c6354f419b50a
     </section>
     <section>2</section>
     <section>3</section>
--- page-contact.php
+++ page-contact.php
@@ -59,18 +59,18 @@ $imagedir = get_template_directory_uri();
                     </div>
                     <div class="contact-frm__inner-row">
                         <div>
-                            <label for="fname">フリガナ※</label>
+                            <label for="fname">貴社名※</label>
                         </div>
                         <div>
-                            <input type="text" id="lname" name="lname" value="" placeholder="ex)ヤマダ">
+                            <input type="text" id="lname" name="lname" value="" placeholder="ex)株式会社ルーフ">
                         </div>
                     </div>
                     <div class="contact-frm__inner-row">
                         <div>
-                            <label for="fname">フリガナ※</label>
+                            <label for="fname">部署・役職名</label>
                         </div>
                         <div>
-                            <input type="text" id="lname" name="lname" value="" placeholder="ex)ヤマダ">
+                            <input type="text" id="lname" name="lname" value="" placeholder="ex)営業部">
                         </div>
                     </div>
                     <div class="contact-frm__inner-row">
@@ -79,64 +79,104 @@ $imagedir = get_template_directory_uri();
                         </div>
                         <div>
                             <small>
-                                <label for="fname">フリガナ※</label>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)ヤマダ">
+                                <label for="fname">アドレス</label>
+                                <input type="text" id="lname" name="lname" value="" placeholder="ex)yamada@xxxxxxx.net">
                             </small>
                             <small>
-                                <label for="fname">フリガナ※</label>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)ヤマダ">
-                            </small>
-                        </div>
-                    </div>
-                    <div class="contact-frm__inner-row">
-                        <div>
-                            <label for="fname">お名前※</label>
-                        </div>
-                        <div>
-                            <small>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)山田">
-                            </small>
-                            <span>-</span>
-                            <small>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)花子">
-                            </small>
-                            <span>-</span>
-                            <small>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)花子">
+                                <label for="fname">確 認 用</label>
+                                <input type="text" id="lname" name="lname" value="" placeholder="ex)yamada@xxxxxxx.net">
                             </small>
                         </div>
                     </div>
                     <div class="contact-frm__inner-row">
                         <div>
-                            <label for="fname">お名前※</label>
+                            <label for="fname">電話番号※</label>
                         </div>
                         <div>
-                            <small><label for="fname">フリガナ※</label></small>
                             <small>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)山田">
+                                <input type="text" id="lname" name="lname" value="" placeholder="ex)0120">
                             </small>
                             <span>-</span>
                             <small>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)花子">
+                                <input type="text" id="lname" name="lname" value="" placeholder="ex)173">
                             </small>
-                            <span></span>
+                            <span>-</span>
                             <small>
+                                <input type="text" id="lname" name="lname" value="" placeholder="ex)602">
+                            </small>
+                        </div>
+                    </div>
+                    <div class="contact-frm__inner-row">
+                        <div>
+                            <label for="fname">ご住所※</label>
+                        </div>
+                        <div>
+                            <small>
+                                <label for="fname">郵便番号</label>
+                                <small>
+                                    <input type="text" id="lname" name="lname" value="" placeholder="ex)100">
+                                    <span>-</span>
+                                    <input type="text" id="lname" name="lname" value="" placeholder="ex)100">
+                                    <a href="">
+                                        <p>住所入力</p>
+                                    </a>
+                                </small>
+                            </small>
+                            <a href="" class="sp-button">
                                 <p>住所入力</p>
+                            </a>
+                            <small>
+                                <label for="">都道府県</label>
+                                <select name="" id="">
+                                    <option value="選択してください">選択してください</option>
+                                </select>
                             </small>
+                            <small>
+                                <label for="">市区郡</label>
+                                <input type="text" placeholder="ex)千代田区">
+                            </small>
+                            <small>
+                                <label for="">町村番地</label>
+                                <input type="text" placeholder="ex)丸の内2-4-1">
+                            </small>
+                            <small>
+                                <label for="">ビル名</label>
+                                <input type="text" placeholder="ex)丸の内ビルディング34F">
+                            </small>
+
                         </div>
                     </div>
                     <div class="contact-frm__inner-row">
                         <div class="bg-h">
-                            <label for="fname">メールアドレス※</label>
+                            <label for="fname">お問い合わせ種別※</label>
                         </div>
                         <div>
                             <small>
-                                <label for="fname">フリガナ※</label>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)ヤマダ">
+                                <select id="cars" name="cars" aria-placeholder="選択してください">
+                                    <option value="volvo">Volvo</option>
+                                    <option value="saab">Saab</option>
+                                    <option value="fiat">Fiat</option>
+                                    <option value="audi">Audi</option>
+                                </select>
+                            </small>
+                        </div>
+                        <!-- <div>
+                            <small>
+                                <label for="郵便番号"></label>
+                                <small>
+                                    <input type="text" placeholder="ex)100">
+                                </small>
+                                <span>-</span>
+                                <span>
+                                    <input type="text" placeholder="ex)6334">
+                                </span>
+                                <small>
+                                    <p>住所入力</p>
+                                </small>
                             </small>
                             <small>
-                                <label for="fname">フリガナ※</label>
-                                <select id="cars" name="cars">
+                                <label for="fname">都道府県</label>
+                                <select id="cars" name="cars" aria-placeholder="選択してください">
                                     <option value="volvo">Volvo</option>
                                     <option value="saab">Saab</option>
                                     <option value="fiat">Fiat</option>
@@ -144,37 +184,44 @@ $imagedir = get_template_directory_uri();
                                 </select>
                             </small>
                             <small>
-                                <label for="fname">フリガナ※</label>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)ヤマダ">
+                                <label for="fname">市区郡</label>
+                                <input type="text" id="lname" name="lname" value="" placeholder="ex)千代田区">
                             </small>
                             <small>
-                                <label for="fname">フリガナ※</label>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)ヤマダ">
+                                <label for="fname">町村番地</label>
+                                <input type="text" id="lname" name="lname" value="" placeholder="ex)丸の内2-4-1">
                             </small>
                             <small>
-                                <label for="fname">フリガナ※</label>
-                                <input type="text" id="lname" name="lname" value="" placeholder="ex)ヤマダ">
+                                <label for="fname">ビル名</label>
+                                <input type="text" id="lname" name="lname" value="" placeholder="ex)丸の内ビルディング34F">
                             </small>
+                        </div> -->
+                    </div>
+                    <div class="contact-frm__inner-row">
+                        <div>
+                            <label for="fname">お問い合わせ内容※</label>
+                        </div>
+                        <div>
+                            <textarea name="" id="" cols="30" rows="10" placeholder="ex)こちらにお書きください"></textarea>
+                        </div>
+                    </div>
+                    <!-- <?php echo do_shortcode('[contact-form-7 id="10" title="Contact form 1"]'); ?> -->
+
+                    <div class="contact-frm__privacy">
+                        <p>「<a href="">プライバシーポリシー</a>」をご確認いただき、同意のうえお問い合わせください。</p>
+                        <div class="contact-frm__privacy--agrmt">
+                            <input type="checkbox"><a href="">プライバシーポリシーに同意する</a>
                         </div>
                     </div>
 
-
-
-
-                    <!-- <?php echo do_shortcode('[contact-form-7 id="10" title="Contact form 1"]'); ?> -->
+                    <a href="" class="c-button">
+                        <input type="submit" value="内容を確認する">
+                        <img src="<?php echo get_template_directory_uri(); ?>/release/image/" alt="">
+                    </a>
                 </form>
 
 
-
-
-
-
             </div>
-
-
-
-
-
             <div class="c-pageTop">
                 <a href="#">
                     <p class="txt">PAGE TOP</p>
