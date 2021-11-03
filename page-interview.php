86098ad22e91e2c042ecbe227c463d4db6d793b7
roi-louise <roi@bpoc.co.jp>
2021-11-03 15:17:38 +0800

    change: update sec1 layout

--- page-interview.php
+++ page-interview.php
@@ -2,72 +2,76 @@
 
 /* Template Name: interview */
 
-get_header();?>
+get_header(); ?>
 
 <div class="page__content interview">
-   <div class="section_1">
-       <div class="l-wrap">
-            <img class="bg-1" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/bg-1.png" alt="">
-            <p class="section_1-ttl">Interview</p>
-            <p class="section_1-sub1">この文章はダミーです<br>この文章はダミーです</p>
-            <p class="section_1-sub2">この文章はダミーですこの文章はダミーですこの文章はダミーです<br>この文章はダミーですこの文章はダミーですこの文章はダミーです</p>
-       </div>
-       <img class="bg-2" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/bg-2.png" alt="">
-
-       <!-- content-1 -->
-        <div class="section_1-cntnt">
+    <div class="section_1">
+        <div class="clipPath">
             <div class="l-wrap">
-                <div class="u-d-f u-jc-sb">
-                    <div class="section_1-cntnt-1">
-                        <p class="section_1-cntnt-1-hdr">この文章はダミーです<br>この文章はダミーです</p>
-                        <p class="section_1-cntnt-1-txt1">Dummy textDummy textDummy textDummy text<br> Dummy textDummy text <br>Dummy textDummy textDummytext</p>
-                        <p class="section_1-cntnt-1-txt2">
-                            この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーです</p>
-                        <div class="section_1-cntnt-1-biography">
-                            <p class="section_1-cntnt-1-biography-hdr">代表取締役 木村武史の略歴</p>
-                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
-                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
-                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                <img class="bg_1" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/bg_1.png" alt="">
+                <img class="bg_2" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/bg_2.png" alt="">
+                <img class="bg_3" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/bg_3.png" alt="">
+
+                <div class="section_1-header">
+                    <p class="section_1-ttl">Interview</p>
+                    <p class="section_1-sub1">この文章はダミーです<br>この文章はダミーです</p>
+                    <p class="section_1-sub2">この文章はダミーですこの文章はダミーですこの文章はダミーです<br>この文章はダミーですこの文章はダミーですこの文章はダミーです</p>
+                </div>
+                <div class="section_1-cntnt">
+                    <div class="u-d-f">
+                        <div class="section_1-cntnt-1">
+                            <p class="section_1-cntnt-1-hdr">この文章はダミーです<br>この文章はダミーです</p>
+                            <p class="section_1-cntnt-1-txt1">Dummy textDummy textDummy textDummy text<br> Dummy textDummy text <br>Dummy textDummy textDummytext</p>
+                            <p class="section_1-cntnt-1-txt2">
+                                この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーです</p>
+                            <div class="section_1-cntnt-1-biography">
+                                <p class="section_1-cntnt-1-biography-hdr">代表取締役 木村武史の略歴</p>
+                                <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                    <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                    <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                                </div>
+                                <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                    <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                    <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                                </div>
+                                <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                    <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                    <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                                </div>
+                                <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                    <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                    <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                                </div>
+                                <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                    <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                    <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                                </div>
+                                <div class="section_1-cntnt-1-biography-cntnt u-d-f">
+                                    <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
+                                    <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                                </div>
                             </div>
-                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
-                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
-                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
-                            </div>
-                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
-                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
-                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
-                            </div>
-                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
-                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
-                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
-                            </div>
-                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
-                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
-                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
-                            </div>
-                            <div class="section_1-cntnt-1-biography-cntnt u-d-f">
-                                <p class="section_1-cntnt-1-biography-cntnt-yr">0000年</p>
-                                <p class="section_1-cntnt-1-biography-cntnt-txt">この文章はダミーです</p>
+                        </div>
+
+                        <div class="section_1-cntnt-2">
+                            <img class="sec1img" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec1_img.png" alt="">
+
+                            <div class="section_1-cntnt-2-bdg">
+                                <p>あたり</p>
                             </div>
                         </div>
                     </div>
-
-                    <div class="section_1-cntnt-2">
-                        <p class="section_1-cntnt-2-txt1">代表取締役</p>
-                        <p class="section_1-cntnt-2-txt2">木村 武史</p>
-                    </div>
-                </div> 
+                </div>
             </div>
-            <img class="section_1-img" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/sec1_img.png" alt="">
         </div>
-   </div>
+    </div>
 
-   <div class="section_2">
+    <div class="section_2">
         <div class="l-wrap">
             <div class="section_2-cntnt">
                 <div class="section_2-cntnt-rows u-d-f u-jc-sb">
                     <div class="section_2-cntnt-rows-1">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/page/interview/sec2_img.png" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2_img.png" alt="">
                     </div>
                     <div class="section_2-cntnt-rows-2">
                         <p class="section_2-cntnt-rows-2-hdr">この文章はダミーです<br>この文章はダミーです</p>
@@ -86,14 +90,14 @@ get_header();?>
                     </div>
                 </div>
 
-                <img class="sec2-bg1" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/sec2-bg1.png" alt="">
-                <img class="sec2-bg2" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/sec2-bg2.png" alt="">
-                
+                <img class="sec2-bg1" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2-bg1.png" alt="">
+                <img class="sec2-bg2" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2-bg2.png" alt="">
+
             </div>
         </div>
-   </div>
+    </div>
 
-   <div class="section_3">
+    <div class="section_3">
         <div class="l-wrap">
             <div class="section_3-cntnt">
                 <div class="section_3-cntnt-rows u-d-f u-jc-sb">
@@ -103,7 +107,7 @@ get_header();?>
                         <p class="section_3-cntnt-rows-1-txt2">この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーです</p>
                     </div>
                     <div class="section_3-cntnt-rows-2">
-                        <img src="<?php echo get_template_directory_uri();?>/release/image/page/interview/sec3-img.png" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec3-img.png" alt="">
                     </div>
                 </div>
                 <div class="u-d-f section_3-cntnt-btns">
@@ -117,10 +121,10 @@ get_header();?>
                     </div>
                 </div>
 
-                <img class="sec3-bg1" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/sec3-bg1.png" alt="">
-                <img class="sec3-bg2" src="<?php echo get_template_directory_uri();?>/release/image/page/interview/sec3-bg2.png" alt="">        
+                <img class="sec3-bg1" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec3-bg1.png" alt="">
+                <img class="sec3-bg2" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec3-bg2.png" alt="">
             </div>
         </div>
-   </div>
-   
+    </div>
+
 </div>
