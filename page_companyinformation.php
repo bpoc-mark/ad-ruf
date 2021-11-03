3b7cfae80d62bb56a93f3e74db76d896f20ca054
kirk-patrik <kirk_patrik@bpoc.co.jp>
2021-11-03 13:59:30 +0800

    create page_companyinformation

---
+++ page_companyinformation.php
@@ -0,0 +1,201 @@
+<?php get_header();
+
+$imagedir = get_template_directory_uri();
+
+?>
+<div class="page__content acc">
+    <div class="acc__section_1">
+        <!-- //CLIP -->
+        <div class="clip-top"></div>
+        <div class="l-wrap">
+            <img class="acc__bg_1" src="<?php echo $imagedir ?>/release/image/page/case/bg_1.png" alt="">
+            <p class="acc__ttl">Access</p>
+            <h1 class="acc__title">丸の内ルーフの<br class="u-d-n-pc">アクセス情報</h1>
+            <p class="acc__sub">東京都の丸の内に構える株式会社ルーフ。<br> 丸の内ルーフまでアクセス情報や最<br class="u-d-n-pc">寄駅からの道順をご紹介いたします。</p>
+        </div>
+
+        <div class="acc__main-content">
+            <div class="l-wrap">
+                <div class="acc__main-content--row">
+
+                    <div class="col col_left">
+                        <div class="infos">
+                            <div class="info">
+                                <div class="info_left il01">
+                                    <div>
+                                        <p>住所</p>
+                                    </div>
+
+                                </div>
+
+                                <div class="info_right ir01">
+                                    <p>〒100-6334　<br class="u-d-n-pc">東京都千代田区丸の内2-4-1<br class="u-d-n-pc">
+                                        丸の内ビルディング34F <br class="u-d-n-pc">3401区<br br01>
+                                        THE PREMIER<br class="u-d-n-pc"> FLOOR</p>
+                                </div>
+                            </div>
+
+                            <div class="info">
+                                <div class="info_left il02">
+                                    <p>最寄り駅</p>
+                                </div>
+
+                                <div class="info_right ir02">
+                                    <p>JR 東京駅…丸の内南口徒歩1分<br>
+                                        【東京メトロ 丸の内線】<br>
+                                        東京駅より直結<br>
+                                        【東京メトロ 千代田線】<br>
+                                        二重橋駅より直結</p>
+                                </div>
+                            </div>
+
+                            <div class="info">
+                                <div class="info_left il03">
+                                    <p>営業時間</p>
+                                </div>
+
+                                <div class="info_right ir03">
+                                    <p>受付時間9：00～18：00</p>
+                                </div>
+                            </div>
+
+                            <div class="info">
+                                <div class="info_left il03">
+                                    <p>定休日</p>
+                                </div>
+
+                                <div class="info_right ir03">
+                                    <p>土日祝</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col col_right">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3240.8300197907433!2d139.7615817!3d35.6811872!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bf9e8d6b29b%3A0xed2d507ceb26bf31!2sMarunouchi%20Building%2C%2034f%2C%202-ch%C5%8Dme-4-1%20Marunouchi%2C%20Chiyoda%20City%2C%20Tokyo%20100-6390%2C%20Japan!5e0!3m2!1sja!2sph!4v1635498881295!5m2!1sja!2sph" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div class="p-cntct">
+
+        <div class="l-wrap">
+            <div class="p-cntct-hdr">
+                <p>contact</p>
+                <p>売却から管理改善まで、<br class="u-d-n-pc">まずはお気軽に<br class="u-d-n-pc">ご相談ください。</p>
+            </div>
+
+            <div class="p-cntct-inqrs">
+                <div class="p-cntct-inqrs-dtl phone">
+                    <a class="p-cntct-inqrs-dtl-link" href="">
+                        <p>お電話でのお問い合わせ</p>
+                        <p>0120-173-602
+                            <br>
+                            受付時間：9：00～18：00<br class="u-d-n-pc">（土日祝除く）
+                        </p>
+                    </a>
+                </div>
+
+                <div class="p-cntct-inqrs-dtl line">
+                    <a class="p-cntct-inqrs-dtl-link" href="">
+                        <p>LINEお問い合わせ</p>
+                        <p>お問い合わせ24時間受付</p>
+                    </a>
+                </div>
+
+                <div class="p-cntct-inqrs-dtl email">
+                    <a class="p-cntct-inqrs-dtl-link" href="">
+                        <p>メールでのお問い合わせ</p>
+                        <p>お問い合わせフォーム<br>24時間受付</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="section_3">
+        <div class="l-wrap pt100">
+            <img class="section_3__bg_1" src="<?php echo $imagedir ?>/release/image/page/case/bg_3.png" alt="">
+            <div class="c-case03__txt section_3__cnt01">
+                <p class="c-case03__txt__ttl acc_ttl">Access</p>
+                <p class="c-case03__txt__sub acc_sub">東京駅から<br class="u-d-n-pc">丸の内ルーフまでのアクセス</p>
+            </div>
+            <img class="section_3__bg_2" src="<?php echo $imagedir ?>/release/image/page/case/bg_2.png" alt="">
+
+
+            <div class="section_3__main-content">
+                <div class="section_3__main-content-row">
+                    <div class="col">
+                        <div class="col_left">
+                            <p>Directions</p>
+                            <p>JR 東京駅<br class="u-d-n-sp">
+                                からの道順</p>
+                        </div>
+
+                        <div class="col_right">
+                            <div class="box col_right-flex">
+                                <img class="" src="<?php echo $imagedir ?>/release/image/page/access/acc_img_1_sp.png" alt="">
+                                <img class="pos" src="<?php echo $imagedir ?>/release/image/page/access/acc_right_arrow.png" alt="">
+                                <p>まずJR東京駅改札を出ていただき、M4入り口から、西方向に都道404号を進んでください。</p>
+                            </div>
+                            <div class="box col_right-flex">
+                                <img class="" src="<?php echo $imagedir ?>/release/image/page/access/acc_img_2_sp.png" alt="">
+                                <img class="pos" src="<?php echo $imagedir ?>/release/image/page/access/acc_right_arrow.png" alt="">
+                                <p>ISSEY MIYAKE MARUNOUCHIの手前で左折し丸の内仲通りを進んでください。</p>
+                            </div>
+                            <div class="box col_right-flex">
+                                <img class="" src="<?php echo $imagedir ?>/release/image/page/access/acc_img_3_sp.png" alt="">
+                                <p>丸の内仲通り左手に丸の内ビルディングが見えます。その34Fが丸の内ルーフです。</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="col_left">
+                            <p>Directions</p>
+                            <p>東京メトロ <br class="u-d-n-sp">
+                                二重橋前駅<br br>
+                                からの道順</p>
+                        </div>
+
+                        <div class="col_right">
+                            <div class="box col_right-flex">
+                                <img src="<?php echo $imagedir ?>/release/image/page/access/acc_img_4_sp.png" alt="">
+                                <img class="pos" src="<?php echo $imagedir ?>/release/image/page/access/acc_right_arrow.png" alt="">
+                                <p>東京メトロ 千代田線「二重橋駅」4番出口から出て直進してください。</p>
+                            </div>
+                            <div class="box col_right-flex">
+                                <img class="" src="<?php echo $imagedir ?>/release/image/page/access/acc_img_5_sp.png" alt="">
+                                <img class="pos" src="<?php echo $imagedir ?>/release/image/page/access/acc_right_arrow.png" alt="">
+                                <p>交差点で左折し丸の内仲通りを進んでください。</p>
+                            </div>
+                            <div class="box col_right-flex">
+                                <img class="" src="<?php echo $imagedir ?>/release/image/page/access/acc_img_6_sp.png" alt="">
+                                <p>丸の内仲通り右手に丸の内ビルディングが見えます。その34Fが丸の内ルーフです。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="l-wrap u-d-f u-d-f-jcc u-mt-60 acc--fd">
+                <a href="#" class="c-buttonCase acc--btn">
+                    <p class="txt">ルーフのオフィス風景を見る</p>
+                    <img src="<?php echo $imagedir ?>/release/image/page/case/btn_arrow.svg" alt="">
+                </a>
+                <a href="#" class="c-buttonCase acc--btn">
+                    <p class="txt">代表メッセージを見る</p>
+                    <img src="<?php echo $imagedir ?>/release/image/page/case/btn_arrow.svg" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<h1>sadsds</h1>
+<?php get_footer(); ?>
