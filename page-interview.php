ddfb009f0ec6785cd79ff2295e6c5212566da48e
roi-louise <roi@bpoc.co.jp>
2021-11-03 22:21:02 +0800

    change: update sp view sec2

--- page-interview.php
+++ page-interview.php
@@ -88,12 +88,12 @@ get_header(); ?>
                             <div class="section_1-cntnt-2-bdg">
                                 <p>あたり</p>
                             </div>
-                        
+
                             <div class="section_1-cntnt-2-rep">
-                                 <div class="section_1-cntnt-2-rep-txt">
+                                <div class="section_1-cntnt-2-rep-txt">
                                     <p>代表取締役</p>
                                     <p>木村 武史</p>
-                                 </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -102,38 +102,45 @@ get_header(); ?>
         </div>
     </div>
 
-    <!-- <div class="section_2">
-        <div class="l-wrap">
-            <div class="section_2-cntnt">
-                <div class="section_2-cntnt-rows u-d-f u-jc-sb">
-                    <div class="section_2-cntnt-rows-1">
-                        <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2_img.png" alt="">
-                    </div>
-                    <div class="section_2-cntnt-rows-2">
-                        <p class="section_2-cntnt-rows-2-hdr">この文章はダミーです<br>この文章はダミーです</p>
-                        <p class="section_2-cntnt-rows-2-txt1">Dummy textDummy textDummy textDummy text <br>Dummy textDummy text<br>Dummy textDummy textDummy text</p>
-                        <p class="section_2-cntnt-rows-2-txt2">この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーです</p>
-                    </div>
-                </div>
-                <div class="u-d-f section_2-cntnt-btns">
-                    <div class="c-buttonCase">
-                        <p class="txt">CARGUY／カーガイについて</p>
-                        <img src="//localhost:3000/wp-content/themes/ad-ruf/release/image/page/case/btn_arrow.svg" alt="">
-                    </div>
-                    <div class="c-buttonCase">
-                        <p class="txt">代表メッセージ</p>
-                        <img src="//localhost:3000/wp-content/themes/ad-ruf/release/image/page/case/btn_arrow.svg" alt="">
-                    </div>
-                </div>
+    <div class="section_2">
+     
+            <div class="l-wrap">
 
                 <img class="sec2-bg1" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2-bg1.png" alt="">
-                <img class="sec2-bg2" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2-bg2.png" alt="">
-
+                <img class="sec2-bg2" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2_bg2.png" alt="">
+                <img class="sec2-bg3" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2_bg3.png" alt="">
+                <img class="sec2-bg1-sp u-d-n-pc" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2_bg1_sp.png" alt="">
+                <img class="sec2-bg2-sp u-d-n-pc" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2_bg2_sp.png" alt="">
+                <div class="section_2-cntnt">
+                    <div class="section_2-cntnt-rows u-d-f u-jc-sb">
+                        <div class="section_2-cntnt-rows-1 u-d-n-sp">
+                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2_img.png" alt="">
+                        </div>
+                        <div class="section_2-cntnt-rows-2">
+                            <p class="section_2-cntnt-rows-2-hdr">この文章はダミーです<br>この文章はダミーです</p>
+                            <p class="section_2-cntnt-rows-2-txt1">Dummy textDummy textDummy textDummy text <br>Dummy textDummy text<br>Dummy textDummy textDummy text</p>
+                            <div class="img-sp u-d-n-pc">
+                                <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec2_img.png" alt="">
+                            </div>
+                            <p class="section_2-cntnt-rows-2-txt2">この文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーですこの文章はダミーです</p>
+                        </div>
+                    </div>
+                    <div class="u-d-f section_2-cntnt-btns">
+                        <div class="c-buttonCase">
+                            <p class="txt">CARGUY／カーガイについて</p>
+                            <img src="//localhost:3000/wp-content/themes/ad-ruf/release/image/page/case/btn_arrow.svg" alt="">
+                        </div>
+                        <div class="c-buttonCase">
+                            <p class="txt">代表メッセージ</p>
+                            <img src="//localhost:3000/wp-content/themes/ad-ruf/release/image/page/case/btn_arrow.svg" alt="">
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+
     </div>
 
-    <div class="section_3">
+    <!-- <div class="section_3">
         <div class="l-wrap">
             <div class="section_3-cntnt">
                 <div class="section_3-cntnt-rows u-d-f u-jc-sb">
@@ -161,6 +168,6 @@ get_header(); ?>
                 <img class="sec3-bg2" src="<?php echo get_template_directory_uri(); ?>/release/image/page/interview/sec3-bg2.png" alt="">
             </div>
         </div>
-    </div> -->
+    </div>  -->
 
 </div>
