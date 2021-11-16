3083e6cd84a7a5025aaaa3fc4c7108ec8e76c3ac
bpoc-denver <denver.gomez@bpoc.co.jp>
2021-11-17 06:38:31 +0800

    correction update on ourenterprises

--- page-our-enterprises.php
+++ page-our-enterprises.php
@@ -72,23 +72,8 @@ $imagedir = get_template_directory_uri();
                     <div class="col heading">
                         <p class="heading01">収益不動産の管理</p>
                         <p class="heading02">Real Estate Management</p>
-                        <p class="heading03 ls007">丸の内ルーフは、豊富なバリューアップの経験を武器に最適な不動<br class="u-d-n-sp">産管理を行い、お預かりした大切な収益不動産の価値を高めます。</p>
+                        <p class="heading03 ls007">丸の内ルーフは、豊富なバリューアップの経験を武器に最適な不動<br class="u-d-n-sp">産管理を行い、<br class="br-sp">お預かりした大切な収益不動産の価値を高めます。</p>
                         <img src="<?php echo $imagedir ?>/release/image/page/ourenterprises/oe-img2.png" alt="">
-                        <!-- <div class="col--box">
-                            <img src="<?php echo $imagedir ?>/release/image/page/ourenterprises/oe-img2.png" alt="">
-                            <p class="top-text01">
-                                <span>R</span>eal Estate
-                            </p>
-                            <p class="top-text02">
-                                <span>M</span>anagement
-                            </p>
-                            <p class="num">02</p>
-                            <div class="info">
-                                <p class="info-ttl">信頼という満足で満たす</p>
-                                <p class="info-sub">RUFの収益不動産の管理</p>
-                            </div>
-                        </div> -->
-
                         <div class="u-d-f u-f-jcc u-pt-30 oe__btns">
                             <a href="#" class="c-buttonCase oe--btn">
                                 <p class="txt">詳細を見る</p>
@@ -103,27 +88,11 @@ $imagedir = get_template_directory_uri();
 
                     </div>
 
-
                     <div class="col heading">
                         <p class="heading01">収益不動産の運用</p>
                         <p class="heading02">Real Estate Operation</p>
                         <img src="<?php echo $imagedir ?>/release/image/page/ourenterprises/oe-img3.png" alt="">
-                        <!-- <div class="col--box">
-                            <img src="<?php echo $imagedir ?>/release/image/page/ourenterprises/oe-img3.png" alt="">
-                            <p class="top-text01">
-                                <span>R</span>eal Estate
-                            </p>
-                            <p class="top-text02">
-                                <span>O</span>peration
-                            </p>
-                            <p class="num">03</p>
-                            <div class="info">
-                                <p class="info-ttl">人生のパートナーとなる</p>
-                                <p class="info-sub">RUFの収益不動産の運用</p>
-                            </div>
-                        </div> -->
-
-                        <p class="heading03 ls007 u-pb-0 u-pt-30">丸の内ルーフは、自社で収益不動産を保有する「プロのオー<br class="u-d-n-sp">ナー」として、首都圏を中心に多数の物件を運用しています。</p>
+                        <p class="heading03 ls007 u-pb-0 u-pt-30">丸の内ルーフは、自社で収益不動産を保有する<br class="br-sp">「プロのオー<br class="u-d-n-sp">ナー」として、<br class="br-sp">首都圏を中心に多数の物件を運用しています。</p>
 
                         <div class="u-d-f u-f-jcc u-pt-30">
                             <a href="#" class="c-buttonCase oe--btn">
@@ -143,22 +112,7 @@ $imagedir = get_template_directory_uri();
                         <p class="heading01">アセットマネジメント</p>
                         <p class="heading02">Asset Management</p>
                         <img src="<?php echo $imagedir ?>/release/image/page/ourenterprises/oe-img4.png" alt="">
-                        <!-- <div class="col--box">
-                            <img src="<?php echo $imagedir ?>/release/image/page/ourenterprises/oe-img4.png" alt="">
-                            <p class="top-text01">
-                                <span>A</span>sset
-                            </p>
-                            <p class="top-text02">
-                                <span>M</span>anagement
-                            </p>
-                            <p class="num">04</p>
-                            <div class="info">
-                                <p class="info-ttl">世代を超えて歴史を紡ぐ</p>
-                                <p class="info-sub">RUFの資産運用(アセットマネジメント)</p>
-                            </div>
-                        </div> -->
-
-                        <p class="heading03 ls007 u-pb-0 u-pt-30">収益不動産に関する豊富なノウハウを活かし、お客様の不動産投資戦略の最適化と資産価値の最大化を目指します。</p>
+                        <p class="heading03 ls007 u-pb-0 u-pt-30">収益不動産に関する豊富なノウハウを活かし、<br class="br-sp">お客様の不動産投資戦略の最適化と<br class="br-sp">資産価値の最大化を目指します。</p>
 
                         <div class="u-d-f u-f-jcc u-pt-30">
                             <a href="#" class="c-buttonCase oe--btn">
@@ -171,17 +125,10 @@ $imagedir = get_template_directory_uri();
                                 <img src="<?= get_template_directory_uri(); ?>/release/image/page/case/btn_arrow.svg" alt="">
                             </a>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-
-
-
-
-
-
     </div>
 </div>
 <?php get_footer(); ?>
