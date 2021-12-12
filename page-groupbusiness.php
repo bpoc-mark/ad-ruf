23a4388c7b7a11224a958697e0620aaa0810b564
gregarsua <arsuagreg@gmail.com>
2021-12-12 13:41:41 +0800

    Change: background, image banner,

--- page-groupbusiness.php
+++ page-groupbusiness.php
@@ -7,21 +7,24 @@ $imagedir = get_template_directory_uri();
 
 <div class="page__content group-business">
     <div class="c-banner">
-        <a href="<?php echo esc_url(home_url('/')); ?>" class="c-ruf-txt"><img src="<?php echo get_template_directory_uri(); ?>/release/image/header/logo.svg" alt="">丸の内 株式会社ルーフ</a>
-            <div class="c-inner">
-                <img class="bnr_pc" src="<?php echo $imagedir ?>/release/image/page/groupbusiness/groupbusiness_bnr.png" alt="">
-                <img class="bnr_sp" src="<?php echo $imagedir ?>/release/image/page/groupbusiness/groupbusiness_bnr_sp.png" alt="">
-                <div class="c-banner--ttl">
-                    <p>グ ル ー プ<br class="bnr_sp"> 事 業</p>
-                    <p>Group <br class="bnr_sp">Business</p>
-                </div>
+        <a href="<?php echo esc_url(home_url('/')); ?>" class="c-ruf-txt"><img
+                src="<?php echo get_template_directory_uri(); ?>/release/image/header/logo.svg" alt="">丸の内 株式会社ルーフ</a>
+        <div class="c-inner">
+            <img class="bnr_pc" src="<?php echo $imagedir ?>/release/image/page/groupbusiness/groupbusiness_bnr.png"
+                alt="">
+            <img class="bnr_sp" src="<?php echo $imagedir ?>/release/image/page/groupbusiness/groupbusiness_bnr_sp.png"
+                alt="">
+            <div class="c-banner--ttl">
+                <p>グ ル ー プ<br class="bnr_sp"> 事 業</p>
+                <p>Group <br class="bnr_sp">Business</p>
             </div>
-            <!-- <div class="bread_crumps">
+        </div>
+        <!-- <div class="bread_crumps">
             <p class="txt">TOP <span>></span> 事 業 内 容 <span>＞</span> 代 表 メ ッ セ ー ジ</p>
         </div> -->
-            <div class="c-bread_crumps">
-                <?php if (function_exists('aioseo_breadcrumbs')) aioseo_breadcrumbs(); ?>
-            </div>
+        <div class="c-bread_crumps">
+            <?php if (function_exists('aioseo_breadcrumbs')) aioseo_breadcrumbs(); ?>
+        </div>
     </div>
     <div class="top-clip"></div>
     <section class="sect_1">
@@ -32,8 +35,7 @@ $imagedir = get_template_directory_uri();
                         Group Business
                     </p>
                     <p class="sect_1__header">
-                        クルマが持つ新しい <br class="u-d-n-pc"> 世界観の創出 <br>
-                        『CARGUY／カーガイ』
+                        クルマが持つ新しい <br class="u-d-n-pc">世界観の創出<br>『CARGUY／カーガイ』
                     </p>
                     <div class="sect_1__inner">
                         <div class="sect_1__inner--left">
@@ -47,21 +49,28 @@ $imagedir = get_template_directory_uri();
                             </p>
                         </div>
                         <div class="sect_1__inner--right">
-                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/groupbusiness/carguy_img.png" alt="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/groupbusiness/carguy_img.png"
+                                alt="">
                         </div>
                     </div>
                     <div class="sect_1__btn--wrap">
-                        <a href="<?php echo get_permalink(get_page_by_path('our-enterprises')) ?>" class="c-buttonCase btn--left">
+                        <a href="<?php echo get_permalink(get_page_by_path('our-enterprises')) ?>"
+                            class="c-buttonCase btn--left">
                             <p class="txt">ルーフのメイン事業内容を見る</p>
-                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/case/btn_arrow.svg" alt="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/case/btn_arrow.svg"
+                                alt="">
                         </a>
 
-                        <a href="https://drive.google.com/drive/folders/1sh9HKxPjXklI2VirmBd-X74hSWeUGuHc" class="c-buttonCase btn--right">
+                        <a href="https://drive.google.com/drive/folders/1sh9HKxPjXklI2VirmBd-X74hSWeUGuHc"
+                            class="c-buttonCase btn--right">
                             <p class="txt">公式サイトへ</p>
-                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/case/btn_arrow.svg" alt="">
+                            <img src="<?php echo get_template_directory_uri(); ?>/release/image/page/case/btn_arrow.svg"
+                                alt="">
                         </a>
                     </div>
-                    <img class="rect-one" src="<?php echo get_template_directory_uri(); ?>/release/image/page/groupbusiness/rect_one.png" alt="">
+                    <img class="rect-one"
+                        src="<?php echo get_template_directory_uri(); ?>/release/image/page/groupbusiness/rect_one.png"
+                        alt="">
                 </div>
             </div>
         </div>
