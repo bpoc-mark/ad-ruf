96da525f675080ecf25c8d2b92b3cc3a93fca9cc
bpoc-denver <denver.gomez@bpoc.co.jp>
2022-03-22 17:49:34 +0800

    result-page added chart details when hover

--- page-result.php
+++ page-result.php
@@ -34,13 +34,40 @@ $imagedir = get_template_directory_uri();
                 <p class="c-case01__sub result_sub">私たちルーフは創業以来、赤字を出すことなく、<br>利益水準を持続的に成長させてきました。</p>
 
                 <div class="section_1__charts u-d-n-sp">
-                    <img src="<?php echo $imagedir ?>/release/image/page/result/chart_1.png" usemap="#image-map">
-
-                    <map name="image-map">
-                        <area alt="第2期　H14.4 218.43" title="第2期　H14.4 218.43" href="" coords="92,397,134,445" shape="rect" style="cursor: default; pointer-events: none;">
+                    <map name="image-map1">
+                        <area target="" alt="H14　第2期　H14.4 218.43" title="H14　第2期　H14.4 218.43" coords="91,92,135,447" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H15　第3期　H15.4 193.72" title="H15　第3期　H15.4 193.72" coords="136,447,179,93" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H16　第4期　H16.4 260.02" title="H16　第4期　H16.4 260.02" coords="224,93,181,446" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H17　第5期　H17.4 251.62" title="H17　第5期　H17.4 251.62" coords="269,445,225,92" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H18　第6期　H18.4 901.7" title="H18　第6期　H18.4 901.7" coords="270,447,314,93" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H19　第7期　H19.4 1292.25" title="H19　第7期　H19.4 1292.25" coords="357,93,315,446" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H20　第8期　H20.4 996.27" title="H20　第8期　H20.4 996.27" coords="402,445,359,95" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H21　第9期　H21.4 873.68" title="H21　第9期　H21.4 873.68" coords="404,93,446,446" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H22　第10期　H22.4 932.3" title="H22　第10期　H22.4 932.3" coords="448,91,491,445" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H23　第11期　H23.4 1030.04" title="H23　第11期　H23.4 1030.04" coords="493,92,536,447" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H24　第12期　H24.4 1104.2" title="H24　第12期　H24.4 1104.2" coords="538,92,581,446" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H25　第13期　H25.4 2356.66" title="H25　第13期　H25.4 2356.66" coords="626,445,583,92" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H26　第14期　H26.4 2688.95" title="H26　第14期　H26.4 2688.95" coords="670,447,628,91" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H27　第15期　H27.4 2352.42" title="H27　第15期　H27.4 2352.42" coords="715,445,672,92" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H28　第16期　H28.4 3003.72" title="H28　第16期　H28.4 3003.72" coords="717,92,761,446" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H29　第17期　H29.4 3869.95" title="H29　第17期　H29.4 3869.95" coords="804,446,763,92" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H30　第18期　H30.4 6153.28" title="H30　第18期　H30.4 6153.28" coords="850,445,806,89" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="R1　第19期　R1.4  7069.44" title="R1　第19期　R1.4  7069.44" coords="893,445,852,90" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="R2　第20期　R2.4  12012.23" title="R2　第20期　R2.4  12012.23" coords="938,445,895,89" shape="rect" style="cursor: pointer;">
                     </map>
-                    <!-- <img src="<?php echo $imagedir ?>/release/image/page/result/chart_1.png" alt=""> -->
-                    <img src="<?php echo $imagedir ?>/release/image/page/result/chart_2.png" alt="">
+                    <img src="<?php echo $imagedir ?>/release/image/page/result/chart_1.png" alt="Chart 1" usemap="#image-map1">
+                    <map name="image-map2">
+                        <area target="" alt="H25　第13期　H25.4 92.18" title="H25　第13期　H25.4 92.18" coords="89,93,193,228" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H26　第14期　H26.4 124.15" title="H26　第14期　H26.4 124.15" coords="300,229,195,94" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H27　第15期　H27.4 69.21" title="H27　第15期　H27.4 69.21" coords="407,229,302,93" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H28　第16期　H28.4 176.03" title="H28　第16期　H28.4 176.03" coords="409,94,514,229" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H29　第17期　H29.4 227.12" title="H29　第17期　H29.4 227.12" coords="517,94,621,230" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="H30　第18期　H30.4 102.7" title="H30　第18期　H30.4 102.7" coords="726,228,623,93" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="R1　第19期　R1.4  228.71" title="R1　第19期　R1.4  228.71" coords="727,92,833,228" shape="rect" style="cursor: pointer;">
+                        <area target="" alt="R2　第20期　R2.4  806.47" title="R2　第20期　R2.4  806.47" coords="836,93,939,229" shape="rect" style="cursor: pointer;">
+                    </map>
+                    </map>
+                    <img src="<?php echo $imagedir ?>/release/image/page/result/chart_2.png" alt="Chart 2" usemap="#image-map2">
                 </div>
 
                 <div class="section_1__charts u-d-n-pc">
